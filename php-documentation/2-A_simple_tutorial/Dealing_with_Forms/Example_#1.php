4acdbb5b14a43941aeb10119ddede5da04763698
lihq1403 <lihaiqing1994@163.com>
2019-09-11 14:10:25 +0800

    处理表单

---
+++ php-documentation/2-A_simple_tutorial/Dealing_with_Forms/Example_#1.php
@@ -0,0 +1,5 @@
+<form action="action.php" method="post">
+    <p>姓名: <input type="text" name="name" /></p>
+    <p>年龄: <input type="text" name="age" /></p>
+    <p><input type="submit" /></p>
+</form>
---
+++ php-documentation/2-A_simple_tutorial/Dealing_with_Forms/Example_#2.php
@@ -0,0 +1,2 @@
+你好，<?php echo htmlspecialchars($_POST['name']); ?>。
+你 <?php echo (int)$_POST['age']; ?> 岁了。
