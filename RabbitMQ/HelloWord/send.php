109eb490609a21b62590fa54108b17a1423b223f
lihq1403 <lihaiqing1994@163.com>
2020-05-15 17:44:50 +0800

    单发送单接收

--- RabbitMQ/HelloWord/send.php
+++ RabbitMQ/HelloWord/send.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // 连接RabbitMQ
 $connection = new \PhpAmqpLib\Connection\AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
