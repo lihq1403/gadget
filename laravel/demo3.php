751bdc4baf5c74364a29ade6e92ec70f1f8bdb55
lihq1403 <lihaiqing1994@163.com>
2020-08-19 10:58:35 +0800

    Contracts契约之面向接口编程

---
+++ laravel/demo3.php
@@ -0,0 +1,44 @@
+<?php
+
+// 定义日志的接口规范
+interface log
+{
+    public function write();
+}
+
+// 文件记录日志
+class FileLog implements Log
+{
+    public function write(){
+        echo 'file log write...';
+    }
+}
+
+// 数据库记录日志
+class DatabaseLog implements Log
+{
+    public function write(){
+        echo 'database log write...';
+    }
+}
+
+class User
+{
+    protected $log;
+
+    public function __construct(Log $log)
+    {
+        $this->log = $log;
+    }
+
+    public function login()
+    {
+        // 登录成功，记录登录日志
+        echo 'login success...';
+        $this->log->write();
+    }
+
+}
+
+$user = new User(new DatabaseLog());
+$user->login();
