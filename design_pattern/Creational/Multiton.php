e3f00470bec40c12f51a2e27d828be84212b71c2
lihq1403 <lihaiqing1994@163.com>
2022-04-15 14:48:26 +0800

    多例模式

--- design_pattern/Creational/Multiton.php
+++ design_pattern/Creational/Multiton.php
@@ -1,18 +1,23 @@
 <?php
 
 /**
- * 多例模式
+ * 多例模式  反面模式
  *
 多例模式是指存在一个类有多个相同实例，而且该实例都是该类本身。这个类叫做多例类。
 多例模式的特点是：
 1、多例类可以有多个实例。
 2、多例类必须自己创建、管理自己的实例，并向外界提供自己的实例。
 多例模式实际上就是单例模式的推广。
+ *
+ * 举例：
+ * 2 个数据库连接器，比如一个是 MySQL ，另一个是 SQLite
+ * 多个记录器（一个用于记录调试消息，一个用于记录错误）
  */
 
 final class Multiton
 {
     const INSTANCE_1 = '1';
+
     const INSTANCE_2 = '2';
 
     /**
@@ -21,33 +26,33 @@ final class Multiton
     private static $instances = [];
 
     /**
-     * 这里私有方法阻止用户随意的创建该对象实例
+     * 这里私有方法阻止用户随意的创建该对象实例.
      */
     private function __construct()
     {
     }
 
-    public static function getInstance(string $instanceName): Multiton
-    {
-        if (!isset(self::$instances[$instanceName])) {
-            self::$instances[$instanceName] = new self();
-        }
-
-        return self::$instances[$instanceName];
-    }
-
     /**
-     * 该私有对象阻止实例被克隆
+     * 该私有对象阻止实例被克隆.
      */
     private function __clone()
     {
     }
 
     /**
-     * 该私有方法阻止实例被序列化
+     * 该私有方法阻止实例被序列化.
      */
     private function __wakeup()
     {
+    }
+
+    public static function getInstance(string $instanceName): Multiton
+    {
+        if (! isset(self::$instances[$instanceName])) {
+            self::$instances[$instanceName] = new self();
+        }
+
+        return self::$instances[$instanceName];
     }
 }
 
