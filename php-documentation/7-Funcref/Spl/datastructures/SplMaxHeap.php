d708f7c205b9828f0afd2f5596f46f1f40540bdc
lihq1403 <lihaiqing1994@163.com>
2022-07-09 16:25:08 +0800

    SplMaxHeap

---
+++ php-documentation/7-Funcref/Spl/datastructures/SplMaxHeap.php
@@ -0,0 +1,12 @@
+<?php
+
+$maxHeap = new SplMaxHeap();
+
+$maxHeap->insert(4);
+$maxHeap->insert(8);
+$maxHeap->insert(1);
+$maxHeap->insert(0);
+
+foreach ($maxHeap as $item) {
+    var_dump($item);
+}
